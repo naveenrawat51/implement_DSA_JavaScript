868156669e18464ebccbb403071ffbb04d85d570
naveenrawat51 <naveenrawat51@gmail.com>
2021-01-16 12:40:29 +0530

    ds ui buttons added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import './styles/main.scss';
 
-const printName = () => {
-    console.log('Hello Naveen 666');
-};
+const getAllBtn = Array.prototype.slice.call(
+    document.querySelectorAll('button')
+);
+const mainHeading = document.getElementById('mainHeading');
 
-printName();
+window.addEventListener('click', (event) => {
+    event.stopPropagation();
+    if (event.target.classList.contains('button')) {
+        getAllBtn.forEach((element) => {
+            if (event.target.value === 'Go To HOME') {
+                element.classList.remove('hide');
+                mainHeading.classList.remove('hide');
+                event.target.classList.add('hide');
+            } else {
+                mainHeading.classList.add('hide');
+                if (
+                    element.value !== event.target.value &&
+                    element.classList.contains('ds-btn')
+                ) {
+                    element.classList.add('hide');
+                }
+                if (element.value === 'Go To HOME') {
+                    element.classList.remove('hide');
+                }
+            }
+        });
+    }
+});
