3e3cc9a2beb2de6eeab433e353b4e5364d4bbda8
naveenrawat51 <naveenrawat51@gmail.com>
2021-01-16 15:17:10 +0530

    ui buttons added, back to home button added, demo complexity table added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import './styles/main.scss';
+import { arrayComplexity } from './DS_types/array';
 
 const getAllBtn = Array.prototype.slice.call(
     document.querySelectorAll('button')
 );
 const mainHeading = document.getElementById('mainHeading');
+const arrayComplexityContainer = document.getElementById(
+    'arrayComplexityContainer'
+);
+const complexityDataRowElement = document.getElementById('complexityData');
+
+function generateRowData(data) {
+    let result = `<td><a href="${data.url}">${data.type}</a></td>`;
+    for (let i = 0; i < data.complexityData.length; i++) {
+        result += `<td><code class="${data.complexityData[i].color}">${data.complexityData[i].complexity}</code></td>`;
+    }
+    return result;
+}
 
 window.addEventListener('click', (event) => {
     event.stopPropagation();
@@ -13,6 +26,7 @@ window.addEventListener('click', (event) => {
                 element.classList.remove('hide');
                 mainHeading.classList.remove('hide');
                 event.target.classList.add('hide');
+                arrayComplexityContainer.classList.add('hide');
             } else {
                 mainHeading.classList.add('hide');
                 if (
@@ -24,6 +38,19 @@ window.addEventListener('click', (event) => {
                 if (element.value === 'Go To HOME') {
                     element.classList.remove('hide');
                 }
+            }
+
+            if (event.target.value === 'ARRAY') {
+                arrayComplexityContainer.classList.remove('hide');
+                complexityDataRowElement.innerHTML = generateRowData(
+                    arrayComplexity
+                );
+            }
+
+            if (event.target.value === 'GRAPH') {
+                arrayComplexityContainer.classList.remove('hide');
+                console.log('naveen: ', generateRowData(myData));
+                complexityDataRowElement.innerHTML = generateRowData(myData);
             }
         });
     }
